76cfa5f44301366733525ddb651521e729cdd4fe
William Niemiec <williamniemiec@hotmail.com>
2021-04-06 21:43:41 -0300

    Tests

--- src/Models/PpcEcGenerator.Data/Coverage.cs
+++ src/Models/PpcEcGenerator.Data/Coverage.cs
@@ -1,30 +1,21 @@
-﻿using PpcEcGenerator.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PpcEcGenerator.Data
+﻿namespace PpcEcGenerator.Data
 {
     public class Coverage
     {
+        //---------------------------------------------------------------------
+        //		Constructor
+        //---------------------------------------------------------------------
         public Coverage(double ppcCoverage, double ecCoverage)
         {
             EdgeCoverage = ppcCoverage;
             PrimePathCoverage = ecCoverage;
         }
 
+
+        //---------------------------------------------------------------------
+        //		Properties
+        //---------------------------------------------------------------------
         public double EdgeCoverage { get; private set; }
         public double PrimePathCoverage { get; private set; }
-
-        //public void Calculate(List<Test> listTestPath, List<Requirement> listReqPpc, List<Requirement> listReqEc)
-        //{
-        //    foreach (Test test in listTestPath)
-        //    {
-        //        EdgeCoverage += ((double)test.newReqEcCovered / (double)listReqEc.Count);
-        //        PrimePathCoverage += ((double)test.newReqPpcCovered / (double)listReqPpc.Count);
-        //    }
-        //}
     }
 }
--- test/PpcEcGenerator.Parse/MetricsParserTest.cs
+++ test/PpcEcGenerator.Parse/MetricsParserTest.cs
@@ -3,9 +3,6 @@ using PpcEcGenerator.IO;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace PpcEcGenerator.Parse
@@ -16,7 +13,14 @@ namespace PpcEcGenerator.Parse
         //		Attributes
         //---------------------------------------------------------------------
         private readonly string projectsFolder;
-        private CoverageFileFinder finder;
+        private string projectName;
+        private string ppcPrefix;
+        private string ecPrefix;
+        private string tpPrefix;
+        private string signature;
+        private IDictionary<string, List<Coverage>> parsingResult;
+        private readonly IDictionary<string, List<Coverage>> expectedResult;
+
 
 
         //---------------------------------------------------------------------
@@ -28,25 +32,104 @@ namespace PpcEcGenerator.Parse
                                 + Path.DirectorySeparatorChar
                                 + "projects"
                                 + Path.DirectorySeparatorChar;
+
+            expectedResult = new Dictionary<string, List<Coverage>>();
         }
 
-
+        
         //---------------------------------------------------------------------
         //		Tests
         //---------------------------------------------------------------------
         [Fact]
         public void TestParse()
         {
-            string joptProject = projectsFolder + "exp4j";
-            MetricsParser parser = new MetricsParser(joptProject);
+            UsingProject("Math - reduced");
+            UsingPpcPrefix("TR_PPC");
+            UsingEcPrefix("TR_EC");
+            UsingTpPrefix("TP_");
+            DoParsing();
+
+            WithSignature("org.apache.commons.math4.analysis.solvers.FieldBracketingNthOrderBrentSolverTest.testConvergenceOnFunctionAccuracy()");
+            ExpectCoverage(0.28, 0.41);
+            WithSignature("org.apache.commons.math4.dfp.DfpTest.testLog10()");
+            ExpectCoverage(2.75, 2.63);
+            WithSignature("org.apache.commons.math4.dfp.DfpTest.testIsZero()");
+            ExpectCoverage(6.5, 5.14);
+            WithSignature("org.apache.commons.math4.ExtendedFieldElementAbstractTest.testLinearCombinationFaFa()");
+            ExpectCoverage(2.83, 4.88);
+            WithSignature("org.apache.commons.math4.ExtendedFieldElementAbstractTest.testMultiplyInt()");
+            ExpectCoverage(9, 9.79);
+
+            AssertParsingIsAsExpected();
+        }
+
+
+        //---------------------------------------------------------------------
+        //		Methods
+        //---------------------------------------------------------------------
+        private void UsingProject(string projectName)
+        {
+            this.projectName = projectName;
+        }
+
+        private void UsingPpcPrefix(string prefix)
+        {
+            ppcPrefix = prefix;
+        }
+
+        private void UsingEcPrefix(string prefix)
+        {
+            ecPrefix = prefix;
+        }
+
+        private void UsingTpPrefix(string prefix)
+        {
+            tpPrefix = prefix;
+        }
+
+        private void DoParsing()
+        {
+            MetricsParser parser = new MetricsParser(projectsFolder + projectName);
 
             CoverageFileFinder finder = new CoverageFileFinder.Builder()
-                .PrimePathCoveragePrefix("TR_PPC")
-                .EdgeCoveragePrefix("TR_EC")
-                .TestPathPrefix("TP_")
+                .PrimePathCoveragePrefix(ppcPrefix)
+                .EdgeCoveragePrefix(ecPrefix)
+                .TestPathPrefix(tpPrefix)
                 .Build();
 
-            IDictionary<string, List<Coverage>> dict = parser.ParseMetrics(finder);
+            parsingResult = parser.ParseMetrics(finder);
+        }
+
+        private void WithSignature(string signature)
+        {
+            this.signature = signature;
+        }
+
+        private void ExpectCoverage(double ppc, double ec)
+        {
+            if (expectedResult.ContainsKey(signature))
+            {
+                expectedResult.TryGetValue(signature, out List<Coverage> expectedCoverage);
+
+                expectedCoverage.Add(new Coverage(ppc, ec));
+            }
+            else
+            {
+                List<Coverage> expectedCoverage = new List<Coverage>();
+                
+                expectedCoverage.Add(new Coverage(ppc, ec));
+
+                expectedResult.Add(signature, expectedCoverage);
+            }
+
+            signature = string.Empty;
+            ppc = 0.0;
+            ec = 0.0;
+        }
+
+        private void AssertParsingIsAsExpected()
+        {
+            Assert.Equal(expectedResult, parsingResult);
         }
     }
 }
